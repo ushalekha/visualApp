e050172bc2da53c68d09cb8e4be7e8857759046f
ushalekha <ushalekha@signiwis.com>
2021-03-31 11:35:18 +0530

    firstchange

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("uitable.controller.View1", {
+			onInit: function () {
+				sap.ui.getCore().getConfiguration().setLanguage("hi");
+				// var i18nModel = new sap.ui.model.resource.ResourceModel({
+
+				// 	bundleUrl : "./i18n/i18n_hi.properties",
+					
+				// 	bundleLocale : "hi"
+					
+				// 	});
+				
+			},
+			onClick:function(oevent){
+				debugger;
+			var a=	oevent.getSource().getRows();
+			}
+		});
+	});
